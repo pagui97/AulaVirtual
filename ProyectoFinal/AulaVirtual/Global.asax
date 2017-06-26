7c7a0cfa76ff4518d7635df4847c28159151dcf2
pagui97 <paguimendez97@gmail.com>
2017-06-26 16:43:14 -0600

    proyecto

---
+++ ProyectoFinal/AulaVirtual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AulaVirtual.MvcApplication" Language="C#" %>
---
+++ ProyectoFinal/AulaVirtual/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AulaVirtual.Startup))]
+namespace AulaVirtual
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
